7eef17fa5161b3c52e59523c7b82b35f14191511
mixassio <mixail.bondarev@gmail.com>
2018-09-04 13:38:44 +0300

    change test

--- __tests__/downloadPage.test.js
+++ __tests__/downloadPage.test.js
@@ -1,6 +1,10 @@
 import os from 'os';
+import fs from 'fs';
+import { promisify } from 'util';
 import nock from 'nock';
 import downloadPage from '../src';
+
+const readFileAsync = promisify(fs.readFile);
 
 nock.disableNetConnect();
 
@@ -11,8 +15,11 @@ describe('HttpRequestPromise', () => {
     const body = 'hello, world';
     nock(host).get('/courses').reply(status, body);
     const tempDir = os.tmpdir();
+    const pathFile = `${tempDir}/hexlet-io-courses.html`;
     const response = await downloadPage('http://hexlet.io/courses', tempDir);
     expect(response.status).toBe(status);
     expect(response.data).toBe(body);
+    const dataFile = await readFileAsync(pathFile, 'utf8');
+    expect(response.data).toBe(dataFile);
   });
 });
