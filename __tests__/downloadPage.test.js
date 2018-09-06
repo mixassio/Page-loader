8a4ae3babcd802428096f80a6dc7b19d35b4bc83
mixassio <mixail.bondarev@gmail.com>
2018-09-06 21:32:42 +0300

    check errors step1

--- __tests__/downloadPage.test.js
+++ __tests__/downloadPage.test.js
@@ -5,8 +5,8 @@ import downloadPage from '../src';
 
 nock.disableNetConnect();
 
-describe('HttpRequestPromise', () => {
-  it('#get', async () => {
+describe('Test function', () => {
+  it('Download page and create html-file', async () => {
     const host = 'http://hexlet.io';
     const status = 200;
     const body = 'hello, world';
@@ -17,7 +17,47 @@ describe('HttpRequestPromise', () => {
     expect(response.status).toBe(status);
     expect(response.data).toBe(body);
     const dataFile = await fs.promises.readFile(pathFile, 'utf8');
-    console.log(dataFile);
     expect(response.data).toBe(dataFile);
+  });
+  it('Download src-files and change html-file', async () => {
+    const host = 'http://ru.hexlet.io';
+    const status = 200;
+    const body = [
+      '<html><head></head><body><div>',
+      '<img src="http://ru.hexlet.io/courses/assets/application1.css" />',
+      '<link href="http://ru.hexlet.io/courses/assets/application3.css" sizes="96x96">',
+      '<img src="http://google.com/learning/assets/application5.css">',
+      '</div><div>',
+      '<img src="http://ru.hexlet.io/courses/assets/application2.css" />',
+      '<link href="http://ru.hexlet.io/courses/assets/application4.css" sizes="128x128">',
+      '<script src="http://ru.hexlet.io/courses/assets/application6.css">',
+      '</div></body></html>'].join('');
+    const bodyLocalLinks = [
+      '<html><head></head><body><div>',
+      '<img src="ru-hexlet-io_files/courses-assets-application1.css" />',
+      '<link href="ru-hexlet-io_files/courses-assets-application3.css" sizes="96x96">',
+      '<img src="google-com_files/learning-assets-application5.css">',
+      '</div><div>',
+      '<img src="ru-hexlet-io_files/courses-assets-application2.css" />',
+      '<link href="ru-hexlet-io_files/courses-assets-application4.css" sizes="128x128">',
+      '<script src="ru-hexlet-io_files/courses-assets-application6.css">',
+      '</div></body></html>'].join('');
+    nock(host).get('/courses').reply(status, body);
+
+    nock(host).get('/courses/assets/application1.css').reply(status, 'hello, world');
+    nock(host).get('/courses/assets/application3.css').reply(status, 'hello, world');
+    nock('http://google.com').get('/learning/assets/application5.css').reply(status, 'hello, world');
+    nock(host).get('/courses/assets/application2.css').reply(status, 'hello, world');
+    nock(host).get('/courses/assets/application4.css').reply(status, 'hello, world');
+    nock(host).get('/courses/assets/application6.css').reply(status, 'hello, world');
+
+    const tempDir = os.tmpdir();
+    const pathFile = `${tempDir}/ru-hexlet-io-courses.html`;
+    const response = await downloadPage('http://ru.hexlet.io/courses', tempDir);
+    expect(response.status).toBe(status);
+    expect(response.data).toBe(body);
+    const dataFile = await fs.promises.readFile(pathFile, 'utf8');
+    expect(response.data).toBe(dataFile);
+    // expect(response.data).toBe(dataFile); //проверка на измененный файл
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,16 +2,40 @@ import fs from 'fs';
 import axios from 'axios';
 import url from 'url';
 import path from 'path';
+import cheerio from 'cheerio';
+import srcDownload from './srcDownload';
+
+const mapping = {
+  img: 'src',
+  script: 'src',
+  link: 'href',
+};
 
 const downloadPage = (linkDownload, pathDirSave) => {
   const { hostname, pathname } = url.parse(linkDownload);
   const nameFileSave = `${[...hostname.split('.'), ...pathname.split('/')].filter(el => el).join('-')}.html`;
   const pathFileSave = path.resolve(pathDirSave, nameFileSave);
+  const nameDirSrcSave = `${[...hostname.split('.')].filter(el => el).join('-')}_files`;
+  const pathDirSrcSave = path.resolve(pathDirSave, nameDirSrcSave);
+  if (!fs.existsSync(pathDirSrcSave)) {
+    fs.mkdirSync(pathDirSrcSave);
+  }
+  let responseLet = '';
   return axios.get(linkDownload)
     .then((response) => {
-      fs.promises.writeFile(pathFileSave, response.data)
-        .catch((errWr) => { console.log(errWr); });
-      return response;
+      responseLet = response;
+      return fs.promises.writeFile(pathFileSave, response.data);
+    }).then(() => {
+      const $ = cheerio.load(responseLet.data);
+      console.log($('script').get().map(el => el.attribs));
+      ['link', 'img', 'script'].map(tag => {
+        $(tag).each(function () { 
+          const linkSrc = $(this).get()[0].attribs[mapping[tag]];
+          console.log(srcDownload(linkSrc, pathDirSrcSave));
+          $(this).attr(mapping[tag], 'bla-bla'); });
+      });
+      console.log($.html());
+      return responseLet;
     }).catch((err) => {
       console.log(err);
     });
